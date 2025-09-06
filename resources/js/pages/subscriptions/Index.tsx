826011f779a0e044ed0a0a2d2439a827e860155b
santyasm <yyasant5@gmail.com>
2025-09-06 14:44:24 -0300

    feat: subscription component page

--- resources/js/pages/subscriptions/Index.tsx
+++ resources/js/pages/subscriptions/Index.tsx
@@ -1,6 +1,7 @@
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
 import AppLayout from '@/layouts/app-layout';
 import { BreadcrumbItem } from '@/types';
-import { usePage } from '@inertiajs/react';
+import { Form, usePage } from '@inertiajs/react';
 
 interface Subscription {
     id: number;
@@ -37,9 +38,31 @@ export default function Index() {
                             <h2 className="text-xl font-semibold">
                                 {subscription.plan.name} - R${subscription.plan.price}
                             </h2>
-                            <button className="mr-4 inline-block w-auto cursor-pointer underline underline-offset-2 hover:text-blue-800">
-                                Cancelar Assinatura
-                            </button>
+
+                            <Dialog>
+                                <DialogTrigger asChild>
+                                    <button className="group relative mr-4 cursor-pointer">
+                                        <span className="relative z-10">Cancelar Assinatura</span>
+                                        <span className="absolute bottom-0 left-0 block h-[2px] w-0 bg-gray-700 transition-all duration-300 ease-in-out group-hover:w-full dark:bg-gray-100" />
+                                    </button>
+                                </DialogTrigger>
+
+                                <DialogContent>
+                                    <DialogHeader>
+                                        <DialogTitle>Tem certeza que deseja cancelar?</DialogTitle>
+                                        <DialogDescription>
+                                            Ao cancelar sua assinatura, você perderá acesso imediato às salas vinculadas ao seu plano. Essa ação não
+                                            poderá ser desfeita.
+                                        </DialogDescription>
+                                    </DialogHeader>
+                                    <Form method="PATCH" action="/subscriptions/cancel" transform={(data) => ({ ...data, id: subscription.id })}>
+                                        <button className="group relative cursor-pointer text-red-600">
+                                            <span className="relative z-10">Sim, cancelar</span>
+                                            <span className="absolute bottom-0 left-0 block h-[2px] w-0 bg-red-600 transition-all duration-300 ease-in-out group-hover:w-full" />
+                                        </button>
+                                    </Form>
+                                </DialogContent>
+                            </Dialog>
                         </div>
                         <span>Início: {subscription.start_date}</span>
                         <span>Fim: {subscription.end_date}</span>
