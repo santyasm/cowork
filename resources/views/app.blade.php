c5f94983f0dd48a2a80dd2f0bf096ccfa3033ff0
santyasm <yyasant5@gmail.com>
2025-09-08 21:27:19 -0300

    styles: add dark favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -42,9 +42,11 @@
 
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-    <link rel="icon" href="/favicon.ico" sizes="any">
-    <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+
     <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+
+    <link rel="icon" href="/favicon.svg" type="image/svg+xml" media="(prefers-color-scheme: light)">
+    <link rel="icon" href="/favicon_dark.svg" type="image/svg+xml" media="(prefers-color-scheme: dark)">
 
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
